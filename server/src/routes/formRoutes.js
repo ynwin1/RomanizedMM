7b6b10ba957c875dc5f581ec6b7a7a1775c76c87
YanNaingWin1 <ynwin1@student.ubc.ca>
2023-12-29 12:28:25 -0800

    apply proper JSON formatting on discord message to be sent back

--- server/src/routes/formRoutes.js
+++ server/src/routes/formRoutes.js
@@ -5,14 +5,19 @@ const router = express.Router();
 const discordWebhook = "https://discord.com/api/webhooks/1190069443238297752/0wL25czXU4Xv2tRBIPwIRJ2djEfEbLqhYhlKcFCmkFkCfq_blx7eD04a3GzLQDXOfiMz"
 
 router.post("/submitForm", async (req, res) => {
-    const discordMessage = req.body.formData;
-    console.log(`Message received in BE - ${discordMessage}`);
+    const formData = req.body.formData;
+    const discordMessage = {
+        content: `Song Name: ${formData.songName}\nArtist: ${formData.artist}\nYouTube Link: ${formData.youtubeLink}\nDetails: ${formData.details}`
+    };
     try {
-        await fetch(discordWebhook, {
+        const response = await fetch(discordWebhook, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: discordMessage
+            body: JSON.stringify(discordMessage)
         });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         res.status(200).json({ message: "Song requested successfully. Stay tuned!" });
     } catch (e) {
         console.error('Error sending message to Discord:', e);
