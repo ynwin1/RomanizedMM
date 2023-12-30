61b7e69042b48099d2fa49a1bf840115e4456a9a
YanNaingWin1 <ynwin1@student.ubc.ca>
2023-12-29 22:11:20 -0800

    add retries for dbConnection

--- server/src/config/dbConnection.js
+++ server/src/config/dbConnection.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+const connectDB = async (retries = 5, delay = 2000) => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected successfully!');
     } catch (err) {
         console.error(err.message);
-        process.exit(1);
+        if (retries === 0) {
+            console.log(`Tried connecting to the database ${retries} but failed.`);
+            process.exit(1);
+        } else {
+            console.log(`Retrying to connect to the database - ${retries - 1} tries left`);
+            setTimeout(() => connectDB(retries - 1, delay), delay);
+        }
     }
 };
 
