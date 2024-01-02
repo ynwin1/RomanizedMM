c5e34a3b8a8f91e6036a87b3d78b2cf562f1e852
YanNaingWin1 <ynwin1@student.ubc.ca>
2024-01-01 19:55:41 -0800

    add animation to display song's about when music box is rendered

--- client/src/components/MusicBox/MusicBox.js
+++ client/src/components/MusicBox/MusicBox.js
@@ -1,30 +1,44 @@
-import React  from "react";
-import {Box, Fade} from "@mui/material";
-import { styled } from "@mui/system";
+import React, {useEffect, useState} from "react";
+import {Fade} from "@mui/material";
+import {useTheme} from "@mui/system";
 import AboutBox from "./AboutBox/AboutBox";
 import ExtLinksBox from "./ExternalLinksBox/ExtLinksBox";
 import LyricsBox from "./LyricsBox/LyricsBox";
-
-const OverallContainer = styled(Box) ({
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center'
-})
-
-const AboutContainer = styled(Box) ({
-    display: 'flex',
-    flexDirection: 'row',
-    marginTop: '1rem',
-    justifyContent: 'space-evenly'
-})
+import {OverallContainer, AboutContainer, AboutTypography} from "./MusicBoxStyling";
 
 function MusicBox(props) {
     const imageLink = props.song.imageLink;
+
+    const theme = useTheme();
+
+    const about = props.song.about;
+    const aboutDelay = 50;
+    const [aboutText, setAboutText] = useState('');
+    const [aboutIndex, setAboutIndex] = useState(0);
+
+    // reset upon change in song
+    useEffect(() => {
+        setAboutText('');
+        setAboutIndex(0);
+    }, [props.song]);
+
+    useEffect(() => {
+        if (aboutIndex < about.length) {
+            const timeout = setTimeout(() => {
+                setAboutText(prevText => prevText + about[aboutIndex]);
+                setAboutIndex(prevIndex => prevIndex + 1);
+            }, aboutDelay);
+
+            return () => clearTimeout(timeout);
+        }
+    }, [aboutIndex, about]);
+
     return (
         <OverallContainer>
             <Fade in={true} timeout={1000}>
                 {imageLink && <img src={imageLink} alt="albumPhoto" className="album-picture"/>}
             </Fade>
+            <AboutTypography theme={theme}>{aboutText}</AboutTypography>
             <AboutContainer>
                 <AboutBox song={props.song} />
                 <ExtLinksBox song={props.song} />
