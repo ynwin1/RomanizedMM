91b1510f5a50da55da1eb2cc23f4e4d3bfe5ae11
YanNaingWin1 <ynwin1@student.ubc.ca>
2024-01-03 18:56:43 -0800

    add responsiveness to Youtube video

--- client/src/components/MusicBox/YoutubePlayer.js
+++ client/src/components/MusicBox/YoutubePlayer.js
@@ -3,10 +3,14 @@ import ReactPlayer from "react-player";
 
 function YoutubePlayer(props) {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center'}}>
-            <ReactPlayer url={props.link} width={'29vw'} height={'36vh'} style={{marginTop: '3rem'}} controls={true}/>
+        <div className="youtube-player">
+            <ReactPlayer url={props.link}
+                         width='100%'
+                         height='100%'
+                         style={{marginTop: '3rem'}}
+                         controls={true}/>
         </div>
     )
-};
+}
 
 export default YoutubePlayer;
