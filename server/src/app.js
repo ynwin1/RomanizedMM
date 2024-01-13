098031e20725069f32661e3007e2823409879d18
ynwin1 <ynwinubc24@gmail.com>
2024-01-13 00:18:08 -0800

    move sitemap xml to frontend

--- server/src/app.js
+++ server/src/app.js
@@ -4,19 +4,14 @@ import express from 'express';
 import cors from 'cors';
 import songRoutes from './routes/songRoutes.js';
 import formRoutes from "./routes/formRoutes.js";
-import * as path from "path";
-import { fileURLToPath } from 'url';
 
 const createApp = () => {
     const app = express();
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
 
     app.use(express.json());
     app.use(cors({
         origin: process.env.FRONTEND_URI
-    }))
-    app.use(express.static(path.join(__dirname, '..', 'public')));
+    }));
 
     app.use('/api', songRoutes);
     app.use('/api', formRoutes);
