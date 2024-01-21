ad5cc189803a5badfa3eefbb7d39ce2494f215cf
ynwin1 <ynwinubc24@gmail.com>
2024-01-20 20:39:21 -0800

    finalize landing page

--- client/src/components/NavBar/NavBarStyling.js
+++ client/src/components/NavBar/NavBarStyling.js
@@ -1,7 +1,7 @@
 import {styled} from "@mui/system";
 import {AppBar, Button, Toolbar, Typography} from "@mui/material";
 
-export const CustomNavBar = styled(AppBar)({
+export const  CustomNavBar = styled(AppBar)({
     background: "linear-gradient(91deg, #6A1B9A 0.47%, #0B3D91 33.58%, #2E1C89 52.2%, #4B0082 66.08%, #FF1493 99.51%) no-repeat", // theme.palette.mode === 'light' ? '#000000':'#FFFFFF', //`linear-gradient(90deg, #434343 0%, #202020 53.2%, #000 100%)`
     boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
     height: '5rem',
