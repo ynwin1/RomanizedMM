c0b7b300ffb69342870fb35de2ba0bdfe95c31fc
ynwin1 <ynwinubc24@gmail.com>
2024-07-24 22:37:24 -0700

    float video when video goes off screen. Still need to figure out draggable

--- client/src/components/MusicBox/YoutubePlayer.js
+++ client/src/components/MusicBox/YoutubePlayer.js
@@ -1,16 +1,39 @@
-import React from "react"
+import React, {useEffect, useRef, useState} from 'react';
+import { useInView } from "react-intersection-observer";
+import Draggable from "react-draggable";
 import ReactPlayer from "react-player";
 
 function YoutubePlayer(props) {
+    const { ref, inView } = useInView({
+        threshold: 0,
+    });
+
     return (
-        <div className="youtube-player">
-            <ReactPlayer url={props.link}
-                         width='100%'
-                         height='100%'
-                         style={{marginTop: '1rem'}}
-                         controls={true}/>
+        <div style={{ overflow: 'hidden' }} ref={ref}>
+            {inView ?
+                <div className="youtube-player">
+                    <ReactPlayer
+                        url={props.link}
+                        width='100%'
+                        height='100%'
+                        style={{marginTop: '1rem'}}
+                        controls={true}
+                    />
+                </div>
+                :
+                <Draggable>
+                    <div className="draggable-player-container">
+                        <ReactPlayer
+                            url={props.link}
+                            width='100%'
+                            height='100%'
+                            controls={true}
+                        />
+                    </div>
+                </Draggable>
+            }
         </div>
-    )
+    );
 }
 
 export default YoutubePlayer;
