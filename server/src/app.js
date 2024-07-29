4babefba1f809771d478d720d9e84993a7df6bca
ynwin1 <ynwinubc24@gmail.com>
2024-07-28 19:41:06 -0700

    remove prerender

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,11 @@ import dotenv from 'dotenv';
 dotenv.config( {path: "C:\\Users\\User\\OneDrive - UBC\\My Projects\\RomanizedMMMusic\\server\\.env" });
 import express from 'express';
 import cors from 'cors';
-import prerender from 'prerender-node';
 import songRoutes from './routes/songRoutes.js';
 import formRoutes from "./routes/formRoutes.js";
 
 const createApp = () => {
     const app = express();
-
-    const prerenderToken = process.env.PRE_RENDER_TOKEN;
-    prerender.set('prerenderToken', prerenderToken);
-    app.use(prerender);
 
     app.use(express.json());
     app.use(cors({
