13637f27bb9a3540b79e202cf8f67b3b6104ca07
ynwin1 <ynwinubc24@gmail.com>
2024-07-29 11:14:43 -0700

    static file fetch

---
+++ client/src/components/SearchBar/SongHost.js
@@ -0,0 +1,41 @@
+import songData1 from '../../lyricsJSON/AMonePin.json';
+import songData2 from '../../lyricsJSON/ASinPyayParTal.json';
+import songData3 from '../../lyricsJSON/Loser.json';
+import songData4 from '../../lyricsJSON/MinAtwatNgar.json';
+import songData5 from '../../lyricsJSON/MinNaeNeePho.json';
+import songData6 from '../../lyricsJSON/MinShiTaeMyoh.json';
+import songData7 from '../../lyricsJSON/MoeMaKhaEainMatKhaYanPyar.json';
+import songData8 from '../../lyricsJSON/PaingShin.json';
+import songData9 from '../../lyricsJSON/PyanLarChainLay.json';
+import songData10 from '../../lyricsJSON/PyanTwayKyi.json';
+import songData11 from '../../lyricsJSON/SaungTwinNway.json';
+import songData12 from '../../lyricsJSON/ShaeSatYanMaShi.json';
+import songData13 from '../../lyricsJSON/ThanYawZin.json';
+import songData14 from '../../lyricsJSON/LatePyar.json';
+import songData15 from '../../lyricsJSON/GaBarASetSet.json';
+import songData16 from '../../lyricsJSON/BarLoNayThayLal.json';
+import songData17 from '../../lyricsJSON/KyayZuuParKwal.json';
+import songData18 from '../../lyricsJSON/Accident.json';
+import songData19 from '../../lyricsJSON/WayThwarLal.json';
+import songData20 from '../../lyricsJSON/SwayTal.json';
+import songData21 from '../../lyricsJSON/Hurt.json';
+import songData22 from '../../lyricsJSON/MoeKharYae.json';
+import songData23 from '../../lyricsJSON/Why.json';
+import songData24 from '../../lyricsJSON/NeeNeeLayNaeWay.json';
+import songData25 from '../../lyricsJSON/SpaceDream.json';
+import songData26 from '../../lyricsJSON/MaMyint.json';
+import songData27 from '../../lyricsJSON/DearCrush.json';
+import songData28 from '../../lyricsJSON/EainPyanChain.json';
+import songData29 from '../../lyricsJSON/SuicideLove.json';
+import songData30 from '../../lyricsJSON/Flashback.json';
+import songData31 from '../../lyricsJSON/TheSein.json';
+
+
+function allSongs() {
+    const combinedSongData = [
+        songData1, songData2, songData3, songData4, songData5, songData6, songData7, songData8, songData9, songData10, songData11, songData12, songData13, songData14, songData15, songData16,
+        songData17, songData18, songData19, songData20, songData21, songData22, songData23, songData24, songData25, songData26, songData27, songData28, songData29, songData30, songData31];
+    return combinedSongData;
+}
+
+export default allSongs;
