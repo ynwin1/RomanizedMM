a171d3a2dc8637110df04ccd4c51ebca9bcbb1fc
ynwin1 <ynwinubc24@gmail.com>
2024-08-06 22:21:22 -0700

    add songs and update sitemap

--- client/src/components/SearchBar/SongHost.js
+++ client/src/components/SearchBar/SongHost.js
@@ -34,12 +34,14 @@ import songData33 from "../../lyricsJSON/Don'tYouKnow.json";
 import songData34 from '../../lyricsJSON/MyawNayMalSaung.json';
 import songData35 from '../../lyricsJSON/Telepunk.json';
 import songData36 from '../../lyricsJSON/AChitThiChin.json';
+import songData37 from '../../lyricsJSON/NgeChitPonePyin.json';
+import songData38 from '../../lyricsJSON/KaungMaLay.json';
 
 function allSongs() {
     const combinedSongData = [
         songData1, songData2, songData3, songData4, songData5, songData6, songData7, songData8, songData9, songData10, songData11, songData12, songData13, songData14, songData15, songData16,
         songData17, songData18, songData19, songData20, songData21, songData22, songData23, songData24, songData25, songData26, songData27, songData28, songData29, songData30, songData31,
-    songData32, songData33, songData34, songData35, songData36];
+    songData32, songData33, songData34, songData35, songData36, songData37, songData38];
     return combinedSongData;
 }
 
