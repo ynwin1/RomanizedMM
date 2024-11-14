fe2081efdd3118374b225771c0a53bb8031e55d6
ynwin1 <ynwinubc24@gmail.com>
2024-11-13 22:09:31 -0800

    add files

--- client/src/components/SearchBar/SongHost.js
+++ client/src/components/SearchBar/SongHost.js
@@ -70,6 +70,8 @@ import songData69 from '../../lyricsJSON/AthelKwelPyaTike.json';
 import songData70 from '../../lyricsJSON/TheingaraThachinTaBote.json';
 import songData71 from '../../lyricsJSON/Sar.json';
 import songData72 from '../../lyricsJSON/MaChoThawAMhanTaYarr.json';
+import songData73 from '../../lyricsJSON/Radio.json';
+import songData74 from '../../lyricsJSON/Her.json';
 
 function allSongs() {
     const combinedSongData = [
@@ -77,7 +79,7 @@ function allSongs() {
         songData17, songData18, songData19, songData20, songData21, songData22, songData23, songData24, songData25, songData26, songData27, songData28, songData29, songData30, songData31,
     songData32, songData33, songData34, songData35, songData36, songData37, songData38, songData39, songData40, songData41, songData42, songData43, songData44, songData45, songData46, songData47,
     songData48, songData49, songData50, songData51, songData52, songData53, songData54, songData55, songData56, songData57, songData58, songData59, songData60, songData61, songData62, songData63,
-    songData64, songData65, songData66, songData67, songData68, songData69, songData70, songData71, songData72];
+    songData64, songData65, songData66, songData67, songData68, songData69, songData70, songData71, songData72, songData73, songData74];
     return combinedSongData;
 }
 
